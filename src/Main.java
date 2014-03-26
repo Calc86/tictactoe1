baa86c85708ca6da9d0c8aa31741a060b7a4cd6c
calc <calc@list.ru>
2014-03-26 20:35:34 +0400

    test for native AI

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import com.tictactoe.Game;
+import com.tictactoe.ai.NativeAI;
 import com.tictactoe.ui.console.*;
 import com.tictactoe.ui.gui.GameButtonView;
 
 public class Main {
     public static void main(String[] args) {
         Game game = Game.getInstance();
+        NativeAI ai = new NativeAI();
 
         //GameConsoleView view = new GameConsoleView();
         //game.setView(view);
@@ -14,7 +16,7 @@ public class Main {
         GameButtonView buttonView = new GameButtonView();
         game.setView(buttonView);
         //game.setInput(buttonView.getInput(),buttonView.getInput());
-        game.setInput(buttonView.getInput(),null);
+        game.setInput(buttonView.getInput(),ai);
 
         game.play();
     }
