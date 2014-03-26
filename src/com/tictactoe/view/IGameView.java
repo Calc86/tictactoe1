f33cbdb01c57c0937d10733d83733f19bd586893
calc <calc@list.ru>
2014-03-26 12:43:22 +0400

    Комментарии

--- src/com/tictactoe/view/IGameView.java
+++ src/com/tictactoe/view/IGameView.java
@@ -4,14 +4,63 @@ import com.tictactoe.model.History;
 import com.tictactoe.model.Player;
 
 public interface IGameView {
+    /**
+     * Объявляем информацию о ходе игрока
+     * @param player
+     */
     void beforeTurn(Player player);
+
+    /**
+     * Вызывается поле получения данных о ходе
+     * @param player
+     * @param x
+     * @param y
+     */
     void onTurn(Player player, int x, int y);  //мы только что сделали ход, необходимо обновить view
+
+
+    /**
+     * Необходимо обновить поле
+     */
     void afterTurn();
+
+    /**
+     * Начало игры (все окна должны быть созданы, объявляем о том, с кем мы играем
+     */
     void onStart();
+
+    /**
+     * Показать историю
+     * @param history
+     */
     void showHistory(History history);
+
+    /**
+     * Конец игры, есть победитель
+     * @param winner
+     */
     void onWin(Player winner);
+
+    /**
+     * Конец игры, Ничья
+     */
     void onDraw();
+
+    /**
+     * Сообщить о конце игры
+     */
     void onEnd();
+
+    /**
+     * Сообщение об ошибке
+     * @param string
+     */
     void onError(String string);
+
+    /**
+     * Информационное сообщение/log
+     * Классом Gamе не вызывается
+     * @param string
+     */
     void onMessage(String string);
 }
