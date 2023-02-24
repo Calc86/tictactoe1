91c06fe4bc9f54595c6729de38de13bedc91ccb3
Gorbachev Andrey <calc@list.ru>
2023-02-24 16:06:01 +0300

    console view disable ui

--- src/Main.java
+++ src/Main.java
@@ -14,16 +14,17 @@ public class Main {
         //game.setInput(view.getInput(),view.getInput());
         //game.setInput(view.getInput(),null);
 
-        GameButtonView buttonView = new GameButtonView();
+//        GameButtonView buttonView = new GameButtonView();
         //game.setView(buttonView);
         //game.setInput(buttonView.getInput(),buttonView.getInput());
         //game.setInput(buttonView.getInput(),ai);
 
         GameMultiView gameMultiView = new GameMultiView();
         gameMultiView.addView(consoleView);
-        gameMultiView.addView(buttonView);
+//        gameMultiView.addView(buttonView);
         game.setView(gameMultiView);
-        game.setInput(buttonView.getInput(),ai);
+        game.setInput(consoleView.getInput(), ai);
+//        game.setInput(buttonView.getInput(),ai);
 
         game.play();
     }
